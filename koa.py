570413dcdf284065ffe5bde49b79c20ca316f5ed
KOA build <koabld@vm-koaserver5.(none)>
2019-07-30 15:40:54 -1000

    Added get_resource_as_string() and jinja global

--- koa.py
+++ koa.py
@@ -4,6 +4,13 @@ from tpx_gui import tpx_gui
 
 #import tpx_gui
 app = Flask(__name__)
+
+def get_resource_as_string(name, charset='utf-8'):
+    with app.open_resource(name) as f:
+        return f.read().decode(charset)
+
+app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
+
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 #establish tpxgui route
@@ -18,6 +25,6 @@ def tpx_status():
 #run on port 50001
 if __name__ == '__main__':
     host = '0.0.0.0'
-    port = 50001
+    port = 50009
     debug = True
     app.run(host=host,port=port,debug=debug)
