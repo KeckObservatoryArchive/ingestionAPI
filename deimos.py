5794a58d6f826a21a1797da0647d8d8bf67fe622
Jeff Mader <jeff.mader@gmail.com>
2020-03-25 10:39:23 -1000

    Added uppercase for instrument

--- deimos.py
+++ deimos.py
@@ -3,4 +3,4 @@ import instrument
 class Deimos(instrument.Instrument):
     def __init__(self, instr, date, statusType, status, statusMessage='NULL'):
         super().__init__(date,statusType, status, statusMessage)
-        self.instr = instr
+        self.instr = instr.upper()
