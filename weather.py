26ad32888fe1d14a3fa24be07274ceab07c794ed
Josh Riley <joshjriley@gmail.com>
2020-06-12 10:35:31 -1000

    fix dev param needed

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import instrument
 
 class Weather(instrument.Instrument):
-    def __init__(self, instr, date, statusType, status, statusMessage='NULL'):
-        super().__init__(date,statusType, status, statusMessage)
+    def __init__(self, instr, date, statusType, status, statusMessage='NULL', dev=False):
+        super().__init__(date,statusType, status, statusMessage, dev)
         self.instr = instr
